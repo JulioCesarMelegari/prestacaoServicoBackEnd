3cac7c9bfa822213adf6c05d2e81089668e45f6f
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2023-09-13 23:48:29 -0300

    created project, add entities client, service

---
+++ src/main/java/backend/PrestServicoApplication.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestServicoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/backend/model/entities/Client.java
@@ -0,0 +1,29 @@
+package backend.model.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 150)
+	private String name;
+	
+	@Column(nullable = false, length = 11)
+	private String cpf;
+	
+	@Column
+	private LocalDate dateRegister;
+
+}
